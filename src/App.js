87eb01847dd3eae82c3527fde48ff9da91d24a1b
Chihab <chihabhk@gmail.com>
2023-12-19 19:10:00 +0100

    Update fonts and styles in App.css, package.json, and index.html. Refactor App.js and add MUI components.

--- src/App.js
+++ src/App.js
@@ -1,112 +1,27 @@
 import "./App.css";
 
+import {
+    AppWrapper,
+    Header,
+    TelContact,
+    CategoriesWrapper,
+    ItemsWrapper,
+} from "./AppStyle.js";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
-
-import { CategoryCard } from "./Components/CategoryCard.jsx";
 import { getAllCategories, selectCategory } from "./features/menuSlice";
-import logo from "./assets/logo.png";
+import { CategoryCard } from "./Components/CategoryCard.jsx";
+import { Button } from "@mui/material";
+import { red } from "@mui/material/colors";
 
-const AppWrapper = styled.div`
-    position: relative;
-    min-width: 100%;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f8f9fa;
-`;
-
-const Header = styled.header`
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    width: 100%;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 1em;
-
-    > img {
-        width: 80%;
-        max-width: 300px;
-        height: 5em;
-        object-fit: cover;
-    }
-`;
-const TelContact = styled.div`
-    /* width: 100%; */
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    padding: 0.5em;
-    gap: 1em;
-    > a {
-        text-decoration: none;
-        color: #007bff;
-        font-weight: bold;
-        font-size: 1em;
-        padding: 0.5em 1em;
-        border-radius: 7px;
-        background-color: #e7f1ff;
-        transition: all 0.3s ease;
-        display: inline-block;
-
-        &:hover {
-            background-color: #cfe2ff;
-            color: #0056b3;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-        }
-    }
-`;
-
-const CategoriesWrapper = styled.section`
-    position: relative;
-    width: 80%;
-    max-width: 100%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    grid-gap: 1.5em;
-    padding: 1em;
-    transition: grid-gap 0.3s ease;
-
-    @media (max-width: 1024px) {
-        grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media (max-width: 768px) {
-        grid-template-columns: repeat(2, 1fr);
-        padding: 0.5em;
-        grid-gap: 1em;
-    }
-
-    @media (max-width: 480px) {
-        grid-template-columns: 1fr;
-    }
-`;
-
-const ItemsWrapper = styled.section`
-    position: relative;
-    width: 90%;
-    max-width: 100%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    grid-gap: 1.5em;
-    padding: 1em;
-    transition: grid-gap 0.3s ease;
-
-    > span {
-        width: 15%;
-        border-radius: 10px;
-        background-color: #007bff;
-        padding: 1em;
-    }
-`;
-
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+    },
+});
 function App() {
     const dispatch = useDispatch();
 
@@ -122,43 +37,48 @@ function App() {
         dispatch(selectCategory({ data: null }));
     };
     return (
-        <AppWrapper>
-            <Header>
-                <img src={logo} alt="LaConcheta-logo" onClick={handleOnClick} />
-                <TelContact>
-                    <label htmlFor="callUs">
-                        ¿Te gustaría reservar una mesa?
-                    </label>
-                    <a href="tel:962121602">LLámanos!</a>
-                </TelContact>
-            </Header>
-            {selectedCategory && selectedCategory.length > 0 ? (
-                <ItemsWrapper>
-                    <span onClick={handleOnClick}>Atrás</span>
-                    {selectedCategory.map((item) => {
-                        return (
-                            <div key={item.name}>
-                                <h6>{item.name}</h6>
-                                <p>{item.description}</p>
-                                <p>{item.price}</p>
-                            </div>
-                        );
-                    })}
-                </ItemsWrapper>
-            ) : (
-                <CategoriesWrapper>
-                    {menuCategories &&
-                        Object.values(menuCategories).map(([key, value]) => {
+        <ThemeProvider theme={theme}>
+            <AppWrapper>
+                <Header>
+                    <h1>La Concheta</h1>
+
+                    <TelContact>
+                        <label htmlFor="callUs">
+                            ¿Te gustaría reservar una mesa?
+                        </label>
+                        <Button href="tel:962121602">LLámanos!</Button>
+                    </TelContact>
+                </Header>
+                {Array.isArray(selectedCategory) ? (
+                    <ItemsWrapper>
+                        <span onClick={handleOnClick}>Atrás</span>
+                        {selectedCategory.map((item) => {
                             return (
-                                <CategoryCard
-                                    category={{ name: key, ...value }}
-                                    key={key}
-                                />
+                                <div key={item.name}>
+                                    <h6>{item.name}</h6>
+                                    <p>{item.description}</p>
+                                    <p>{item.price}</p>
+                                </div>
                             );
                         })}
-                </CategoriesWrapper>
-            )}
-        </AppWrapper>
+                    </ItemsWrapper>
+                ) : (
+                    <CategoriesWrapper>
+                        {menuCategories &&
+                            Object.values(menuCategories).map(
+                                ([key, value]) => {
+                                    return (
+                                        <CategoryCard
+                                            category={{ name: key, ...value }}
+                                            key={key}
+                                        />
+                                    );
+                                }
+                            )}
+                    </CategoriesWrapper>
+                )}
+            </AppWrapper>
+        </ThemeProvider>
     );
 }
 
