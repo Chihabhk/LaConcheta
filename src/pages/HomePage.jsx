5444a9264259e35b3184c598c1f13c55d00db741
Chihab <chihabhk@gmail.com>
2024-01-18 21:03:52 +0100

    Add Swiper library and update menu images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useDocumentTitle } from "../hooks/useDocumentTitle";
 import {
     Typography,
@@ -9,24 +10,73 @@ import {
     CardActions,
     Button,
 } from "@mui/joy";
+import { register } from "swiper/element/bundle";
+import { EffectFade } from "swiper/modules";
+import "swiper/css";
 
 const HomePage = () => {
+    const navigate = useNavigate();
     useDocumentTitle("Saborea la Auténtica Cocina Mediterránea en LaConcheta");
     const { menuCategories } = useSelector((state) => state.menu);
 
+    useEffect(() => {
+        // Register Swiper web component
+        register();
+    }, []);
+
     return (
-        <>
-            <Typography level="h2" component="h1" gutterBottom>
+        <div style={{ padding: ".4em" }}>
+            <swiper-container
+                slides-per-view="1"
+                modules={[EffectFade]}
+                effect="coverflow"
+                centered-slides="true"
+                autoplay="true"
+                speed={1000}
+                Scrollbar="true"
+                // pagination="true"
+                // pagination-dynamic-bullets="true"
+                style={{ height: "auto" }}>
+                {menuCategories.map((category, key) => {
+                    return (
+                        <swiper-slide
+                            key={key}
+                            onClick={() => {
+                                navigate(`/categories/${category[0]}`);
+                            }}>
+                            <img
+                                style={{
+                                    height: "200px",
+                                    width: "100%",
+                                    objectFit: "cover",
+                                    objectPosition: "center",
+                                }}
+                                src={category[1].url}
+                                alt={
+                                    category[0] + " del restaurante La Concheta"
+                                }
+                            />
+                            <Typography
+                                alignSelf={"center"}
+                                level="h3"
+                                sx={{ mb: 2 }}>
+                                {category[0]}
+                            </Typography>
+                        </swiper-slide>
+                    );
+                })}
+            </swiper-container>
+            <Typography level="h1" component="h1" gutterBottom>
                 Bienvenido a nuestro restaurante La Concheta
             </Typography>
-            <Typography level="body1" gutterBottom>
+            <Typography level="body-lg" gutterBottom>
                 Ofrecemos una variedad de deliciosos platos para satisfacer
                 todos los gustos.
             </Typography>
-            <Typography level="h4" component="h2" gutterBottom>
+            {/* <Typography level="h4" component="h2" gutterBottom>
                 Nuestras categorías
-            </Typography>
-            <Grid container>
+            </Typography> */}
+            {/* <Grid container>
                 {Object.values(menuCategories).map(
                     ([categoryName, categoryData]) => (
                         <Grid xs={12} sm={6} md={4} key={categoryName}>
@@ -60,11 +110,11 @@ const HomePage = () => {
                         </Grid>
                     )
                 )}
-            </Grid>
+            </Grid> */}
             <Typography level="h4" component="h2" gutterBottom>
                 Lo que dicen nuestros clientes
             </Typography>
-        </>
+        </div>
     );
 };
 export default HomePage;
