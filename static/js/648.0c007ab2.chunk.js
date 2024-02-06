a4d76f4ba5eb67aefdf833faf12884b2eea774e5
Chihab <chihabhk@gmail.com>
2024-02-06 17:38:47 +0100

    Updates

---
+++ static/js/648.0c007ab2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklaconcheta=self.webpackChunklaconcheta||[]).push([[648],{2648:(e,a,l)=>{l.r(a),l.d(a,{default:()=>t});var n=l(9060),c=l(2496);const s=(0,n.lazy)((()=>Promise.all([l.e(832),l.e(356),l.e(606),l.e(160),l.e(576)]).then(l.bind(l,576)))),t=e=>{let{children:a}=e;return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(s,{}),a]})}}}]);
+//# sourceMappingURL=648.0c007ab2.chunk.js.map
