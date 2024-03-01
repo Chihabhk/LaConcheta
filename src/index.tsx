9ed0de70d8f7160f2a353ea3cb98c0b85f55a1dc
Chihab <chihabhk@gmail.com>
2024-03-01 16:43:26 +0100

    Actualización del esquema de colores en el tema MUI

--- src/index.tsx
+++ src/index.tsx
@@ -8,19 +8,10 @@ import { extendTheme } from "@mui/joy/styles";
 
 const theme = extendTheme({
     colorSchemes: {
-        dark: {
+        light: {
             palette: {
-                primary: {
-                    50: "#C0CCD9",
-                    100: "#A5B8CF",
-                    200: "#6A96CA",
-                    300: "#4886D0",
-                    400: "#2178DD",
-                    500: "#096BDE",
-                    600: "#1B62B5",
-                    700: "#265995",
-                    800: "#2F4968",
-                    900: "#2F3C4C",
+                warning: {
+                    500: "#df9a66",
                 },
             },
         },
@@ -30,7 +21,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <CssVarsProvider theme={theme}>
+            <CssVarsProvider
+                defaultMode="system"
+                theme={theme}
+                disableNestedContext>
                 <App />
             </CssVarsProvider>
         </Provider>
