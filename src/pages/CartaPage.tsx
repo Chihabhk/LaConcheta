a92a9b9868533b3ed228156cb2f8ba1711e6c842
Chihab <chihabhk@gmail.com>
2024-03-06 18:46:47 +0100

    Actualizar biblioteca Swiper y agregar navegación a la página de la carta

--- src/pages/CartaPage.tsx
+++ src/pages/CartaPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import ItemCard from "../Components/ItemCard.tsx";
 import { MenuState } from "../types";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { Typography } from "@mui/joy";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 
@@ -20,15 +21,16 @@ const CartaPage = () => {
                 <>
                     <Swiper
                         style={{ padding: ".4em" }}
-                        modules={[Pagination]}
+                        modules={[Pagination, Navigation]}
                         spaceBetween={50}
                         slidesPerView={1}
-                        autoHeight={true}>
+                        autoHeight={true}
+                        navigation={true}
+                        navigation-next-el=".custom-next-button"
+                        navigation-prev-el=".custom-prev-button">
                         {menuCategories.map((category) => (
                             <SwiperSlide key={category.id}>
                                 <Typography
-                                    startDecorator={<ArrowBack />}
-                                    endDecorator={<ArrowForward />}
                                     level="h2"
                                     sx={{
                                         justifyContent: "center",
@@ -41,6 +43,12 @@ const CartaPage = () => {
                                 ))}
                             </SwiperSlide>
                         ))}
+                        <div className="pagination-next-button">
+                            <ArrowBack />
+                        </div>
+                        <div className="pagination-prev-button">
+                            <ArrowForward />
+                        </div>
                     </Swiper>
                 </>
             )}
